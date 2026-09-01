fd104e5efc4c0ff1dd81f6ffdba3b00c3dc473d8
crsvt <crsvt.dev@gmail.com>
2026-09-01 10:20:33 +0530

    drivers: usb: Import Xiaomi changes

--- drivers/usb/pd/policy_engine.c
+++ drivers/usb/pd/policy_engine.c
@@ -10,6 +10,7 @@
  * GNU General Public License for more details.
  */
 
+//#define DEBUG
 #include <linux/completion.h>
 #include <linux/delay.h>
 #include <linux/hrtimer.h>
@@ -361,6 +362,7 @@ struct usbpd {
 	struct device		dev;
 	struct workqueue_struct	*wq;
 	struct work_struct	sm_work;
+	struct delayed_work 	vbus_work;//hguan
 	struct hrtimer		timer;
 	bool			sm_queued;
 
@@ -2848,7 +2850,6 @@ static int psy_changed(struct notifier_block *nb, unsigned long evt, void *ptr)
 	}
 
 	typec_mode = val.intval;
-
 	ret = power_supply_get_property(pd->usb_psy,
 			POWER_SUPPLY_PROP_PE_START, &val);
 	if (ret) {
@@ -2856,7 +2857,6 @@ static int psy_changed(struct notifier_block *nb, unsigned long evt, void *ptr)
 				ret);
 		return ret;
 	}
-
 	/* Don't proceed if PE_START=0 as other props may still change */
 	if (!val.intval && !pd->pd_connected &&
 			typec_mode != POWER_SUPPLY_TYPEC_NONE)
@@ -2868,7 +2868,6 @@ static int psy_changed(struct notifier_block *nb, unsigned long evt, void *ptr)
 		usbpd_err(&pd->dev, "Unable to read USB PRESENT: %d\n", ret);
 		return ret;
 	}
-
 	pd->vbus_present = val.intval;
 
 	ret = power_supply_get_property(pd->usb_psy,
@@ -2877,7 +2876,6 @@ static int psy_changed(struct notifier_block *nb, unsigned long evt, void *ptr)
 		usbpd_err(&pd->dev, "Unable to read USB TYPE: %d\n", ret);
 		return ret;
 	}
-
 	pd->psy_type = val.intval;
 
 	/*
@@ -2968,7 +2966,6 @@ static int psy_changed(struct notifier_block *nb, unsigned long evt, void *ptr)
 				typec_mode);
 		break;
 	}
-
 	/* queue state machine due to CC state change */
 	kick_sm(pd, 0);
 	return 0;
@@ -3724,6 +3721,84 @@ static ssize_t get_battery_status_show(struct device *dev,
 }
 static DEVICE_ATTR_RW(get_battery_status);
 
+
+/*hguan add*/
+struct usbpd *pd_lobal;
+unsigned int pd_vbus_ctrl;
+
+module_param(pd_vbus_ctrl, uint, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(pd_vbus_ctrl, "PD VBUS CONTROL");
+
+void pd_vbus_reset(struct usbpd *pd)
+{
+	if (!pd)
+	{
+		pr_err("pd_vbus_reset, pd is null\n");
+		return;
+	}
+	if (pd->vbus_enabled) {
+		regulator_disable(pd->vbus);
+		pd->vbus_enabled = false;
+		if (0 == pd_vbus_ctrl) pd_vbus_ctrl = 500;
+		msleep(pd_vbus_ctrl);
+		enable_vbus(pd);
+	}
+	else
+	{
+		pr_err("pd_vbus is not enabled yet\n");
+	}
+}
+
+/* Handles VBUS off on */
+void usbpd_vbus_sm(struct work_struct *w)
+{
+	struct usbpd *pd = pd_lobal;
+
+	pr_err("usbpd_vbus_sm handle state %s, vbus %d\n",
+	usbpd_state_strings[pd->current_state],pd->vbus_enabled);
+
+	pd_vbus_reset(pd);
+	//to be done, pd->sm_queued = false;
+}
+void kick_usbpd_vbus_sm(void)
+{
+	 pm_stay_awake(&pd_lobal->dev);
+	 //to be done pd_lobal->sm_queued = true;
+	 pr_err("kick_usbpd_vbus_sm handle state %s, vbus %d\n",
+	 usbpd_state_strings[pd_lobal->current_state],pd_lobal->vbus_enabled);
+	 queue_delayed_work(pd_lobal->wq, &(pd_lobal->vbus_work), msecs_to_jiffies(400));
+}
+
+static ssize_t pd_vbus_show(struct device *dev, struct device_attribute *attr,
+ char *buf)
+{
+	struct usbpd *pd = dev_get_drvdata(dev);
+	pr_err("pd_vbus_show handle state %s, vbus %d\n",
+	usbpd_state_strings[pd_lobal->current_state],pd_lobal->vbus_enabled);
+	pd_vbus_reset(pd);
+
+	return 0;
+}
+
+static ssize_t pd_vbus_store(struct device *dev,
+ struct device_attribute *attr, const char *buf, size_t size)
+{
+	int val = 0;
+	//struct usbpd *pd = dev_get_drvdata(dev);
+	if (sscanf(buf, "%d\n", &val) != 0)
+	{
+		pr_err("pd_vbus_store input err\n");
+	}
+	pr_err("pd_vbus_store handle state %s, vbus %d,val %d\n",
+	usbpd_state_strings[pd_lobal->current_state],pd_lobal->vbus_enabled,val);
+	kick_usbpd_vbus_sm();
+
+	return size;
+}
+static DEVICE_ATTR_RW(pd_vbus);
+
+/*hguan add end*/
+
 static struct attribute *usbpd_attrs[] = {
 	&dev_attr_contract.attr,
 	&dev_attr_initial_pr.attr,
@@ -3748,6 +3823,7 @@ static struct attribute *usbpd_attrs[] = {
 	&dev_attr_rx_ado.attr,
 	&dev_attr_get_battery_cap.attr,
 	&dev_attr_get_battery_status.attr,
+	&dev_attr_pd_vbus.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(usbpd);
@@ -3862,6 +3938,7 @@ struct usbpd *usbpd_create(struct device *parent)
 		goto del_pd;
 	}
 	INIT_WORK(&pd->sm_work, usbpd_sm);
+	INIT_DELAYED_WORK(&pd->vbus_work,usbpd_vbus_sm);//hguan
 	hrtimer_init(&pd->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	pd->timer.function = pd_timeout;
 	mutex_init(&pd->swap_lock);
@@ -3985,7 +4062,7 @@ struct usbpd *usbpd_create(struct device *parent)
 
 	/* force read initial power_supply values */
 	psy_changed(&pd->psy_nb, PSY_EVENT_PROP_CHANGED, pd->usb_psy);
-
+	pd_lobal = pd;
 	return pd;
 
 del_inst:
